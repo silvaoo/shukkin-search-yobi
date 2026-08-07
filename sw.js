62757d2d77b73ffcd3236b856ca8d15463d87fb9
silvaoo <silvaoo@users.noreply.github.com>
2026-08-07 07:07:11 +0000

    A:出勤退勤カウントダウン B:結果パラパラ表示 C:期間合計労働時間 を追加 (sw v40)

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 // キャッシュ名にバージョンを入れておき、更新のたびにこの値を変えることで
 // 新しいService Workerが「更新あり」と判定されるようにする
-const CACHE_VERSION = 'yobi-shukkin-v39';
+const CACHE_VERSION = 'yobi-shukkin-v40';
 const CACHE_FILES = [
     './',
     './index.html',
