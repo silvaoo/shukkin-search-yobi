57eadc47a0bbd67c0f8c6bfeaedba66227b13dc3
silvaoo <silvaoo@users.noreply.github.com>
2026-08-25 04:57:24 +0000

    master.htmlから生成する方式に統一 / PDF・画像の置き場所統一 / 更新履歴を分離 / 版2.33 (by AIタケタニ)

--- sw.js
+++ sw.js
@@ -10,10 +10,11 @@
 
 // キャッシュ名にバージョンを入れておき、更新のたびにこの値を変えることで
 // 新しいService Workerが「更新あり」と判定されるようにする
-const CACHE_VERSION = 'yobi-shukkin-v90';
+const CACHE_VERSION = 'yobi-shukkin-v91';
 const CACHE_FILES = [
     './',
     './index.html',
+    './logs.html',      // 更新履歴。別ファイルに分けてある
     './dia-yobi.json',        // ダイヤのデータ。圏外でも使えるよう控えておく
     './mascot.png',
     './icons/icon-192.png',
